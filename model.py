1418e91b9e85c03c359b3a07166447552a984ed6
pbakshihub <pradhumnabakshi10@gmail.com>
2026-09-05 16:01:35 +0530

    Step 29: sum_keepdims

--- model.py
+++ model.py
@@ -248,8 +248,14 @@ def transpose_matrix(arr):
     return arr.T
     pass
 
-# Step 29 - sum_keepdims (not yet solved)
-# TODO: implement
+# Step 29 - sum_keepdims
+import numpy as np
+
+def sum_keepdims(arr, axis):
+    """Sum along `axis` while keeping that dimension as size 1."""
+    # TODO: sum along the given axis preserving the reduced dim as size 1
+    return arr.sum(axis=axis, keepdims=True)
+    pass
 
 # Step 30 - naive_softmax_1d (not yet solved)
 # TODO: implement
